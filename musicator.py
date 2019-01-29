6e84231c77403b2623b84430c8372cc478c2f879
TR0N <theo.reichert7@gmail.com>
2019-01-29 13:45:31 +0100

    290118

--- musicator.py
+++ musicator.py
@@ -2,7 +2,6 @@ import os
 # from typing import Union
 # import sqlite3
 from pathfinder import pathfinder
-# here is the os.walk
 # from extractor import extractor
 # from scanner import scanner
 # from databaseextender import databaseextender
@@ -11,23 +10,15 @@ from pathfinder import pathfinder
 
 language = input("Enter de for German or en for English: ")
 musicdirpath = pathfinder(language)
-print(musicdirpath)
+print("Dein  Musikordner befidnet sich in: " + str(musicdirpath))
 
-files = []
 for dirpath, dirnames, filenames in os.walk(musicdirpath):
         print("Pfad: ", dirpath)
         print("Ordner: ", dirnames)
         print("Datein: ", filenames)
-        files.append(filenames)
-        print()
-print(files)
-
-# mp3header = extractor(chosen_file)
-# # print(mp3header)
-
-# metadata = scanner(mp3header)  # may be problems with var as it gets a tuple as value send back by parser()
-# print(str(metadata[0]) + str(metadata[1]))  # checking
-# databaseextender(metadat[0])
-# renamer(metadata[0], metadata[1])#looks frankenstine
-# yet not thought about much
-# directorator()
+        # for files in filenames:
+        #         mp3header = extractor(files)
+        #         metadata = scanner(mp3header)
+        #         databaseextender(metadata)
+        #         renamer(metadata)
+        #         directorator(metadata)
