1cb32ecd4d4d87b6cfcad70bec5c6f48d297a4e6
Theo Reichert <theo.reichert7@gmail.com>
2019-02-19 02:06:33 +0100

    2.3

--- databasecreator.py
+++ databasecreator.py
@@ -1,8 +1,12 @@
 import sqlite3
 
+liste = ["2Cellos", "ACDC", "ZZ Top"]
 artistsdatabaseobject = sqlite3.connect("artists.db")
 pointer = artistsdatabaseobject.cursor()
-pointer.execute("create table artists_table (artist)")
-# pointer.execute("insert into artists_table values (?)", liste)
+# pointer.execute("create table artists_table (num, artist)")
+for i in liste:
+    num = liste.index(i)
+    comp = [num, i]
+    pointer.execute("insert into artists_table values (?, ?)", comp)
 artistsdatabaseobject.commit()
 artistsdatabaseobject.close()
