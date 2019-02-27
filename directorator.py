55561c745dbbcc831db0752e377185c34aa6de4c
TR0N <theo.reichert7@gmail.com>
2019-02-27 13:21:49 +0100

    6.0

--- directorator.py
+++ directorator.py
@@ -4,10 +4,11 @@ import pathlib
 
 def directorator(musicdirpath,filename,path,interprete):
     os.chdir(musicdirpath)
-    destination = pathlib.PurePath(musicdirpath).joinpath(interprete)
+    destination = pathlib.PurePath(musicdirpath).joinpath(str(interprete))
     if os.path.isdir(destination) == True:
         copyfile(musicdirpath,destination)
         # copy file with the passes name to this folder
     else:
         os.mkdir(interprete)
+        copyfile(musicdirpath,destination)
         #copy files with the passed name into that folder
