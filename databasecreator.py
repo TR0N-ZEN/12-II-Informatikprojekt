21e6ced5f308ad1117c7bcdc8279799920bb8906
TR0N <theo.reichert7@gmail.com>
2019-02-27 13:14:59 +0100

    5.0

--- databasecreator.py
+++ databasecreator.py
@@ -3,7 +3,7 @@ import sqlite3
 liste = ["2Cellos", "ACDC", "ZZ Top", "Brother Dege","Volbeat","Linkin Park","Billy Talent","3 Doors Down","Franz Schubert","Bastille","Eisbrecher","Rammstein","InExtremo","Avicii","Mucc","Parov Stellar","Johnny Cash","Nickelback","George Ezra","The Dead South","Vicetone","Unlike Pluto","ABBA","The Animals","Beatles","BB King","David Guetta","Aerosmith","Saleri","Kaleo","Mumford and Sons","Sportfreunde Stiller","!panic at the disco","Saltatio mortis","Händel"]
 artistsdatabaseobject = sqlite3.connect("artists.db")
 pointer = artistsdatabaseobject.cursor()
-#pointer.execute("create table artists_table (num, artist)")
+pointer.execute("create table artists_table (num, artist)")
 for i in liste:
     num = len(i)
     comp = [num, i]
--- experimental.py
+++
@@ -1,7 +0,0 @@
-import os
-
-langdict = {
-        "de": "/Musik",
-        "en": "/Music"
-}
-print(langdict["de"])
