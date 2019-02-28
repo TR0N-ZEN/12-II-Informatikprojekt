4650fd034cc8fc2ed82f38df147ea28d9b6278d2
Theo Reichert <theo.reichert7@gmail.com>
2019-02-28 22:29:06 +0100

    15.0

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-a = "hello"
-letters = a.split()
-print(letters)
+x = "hello mate"
+y = "hello there"
+a = 0
+o = 1
+while a < len(x): 
+    while o <= len(x):
+        print(y.find(x[a:o]))
+        o = o + 1 
+    a = a + 1 
+    o = 0
