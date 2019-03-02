70d35d9e28f3d3b11ae7298db377a07e78912867
TR0N <theo.reichert7@gmail.com>
2019-03-02 12:49:09 +0100

    16.0

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
-x = "hello mate"
-y = "hello there"
-a = 0
-o = 1
-while a < len(x): 
-    while o <= len(x):
-        print(y.find(x[a:o]))
-        o = o + 1 
-    a = a + 1 
-    o = 0
+a = "Avicii - Addicted to you"
+b = "Avicii - Hey Brother"
+c = "Avicii - For a better day"
+filenames = [a, b, c]
+pieces = []
+def top(filenames):
+    for e in filenames:
+        z = e.split(" ")
+        pieces.append(z)
+
+count = 0
+def one_under_top(filenames, pieces):
+    for e in filenames:
+        for r in pieces:
+            if pieces.index(r) != filenames.index(e):
+                for t in r:
+                    if e.find(t) != -1:
+                        if t != "-":
+                            print(t)
+
+top(filenames)
+one_under_top(filenames, pieces)
+
+if count > 5:
+    print("Found Artist" + str() + " with high probability")
