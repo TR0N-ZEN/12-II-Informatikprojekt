483eca0f1b0b23da9da01729a860563be4918e7f
Theo <theo.reichert7@gmail.com>
2019-03-17 17:59:09 +0100

    22.5

--- musicator.py
+++ musicator.py
@@ -53,23 +53,22 @@ global not_necessary_to_query
 not_necessary_to_query = []
 
 def analyzer(f,r,compound_guess,nextpart,count,legacy):
-        if compound_guess not in not_necessary_to_query and f.find(compound_guess) != -1 and count != len(r):
+        if compound_guess not in not_necessary_to_query and f.find(compound_guess) != -1 and count < len(r):
                 count = count + 1
                 i = r.index(nextpart) # getting index of nextpart
                 nextpart = r[i+1] # redefining nextpart as the item following the current nextpart in list r
                 legacy = compound_guess # savin the confirmed / approved compound_guess for next recursion so that if it fails it will go to else and return legacy, the last ompound_guess that worked
                 compound_guess = compound_guess + " " + nextpart
                 analyzer(f,r,compound_guess,nextpart,count,legacy)
-        else:
-                if count == 3:
-                        q = input("Is " + legacy + " an artist?\n y/n")
-                        if q == "y":
-                                print("Nice we´ll be creating a folder for you")
-                                # add artist to database inorder to run comparison with database again and copy all those whose artist was just added
-                                databaseextender(legacy) # MODULE
-                                # directorator(musicdirpath,f,musicdirpath,legacy) - bad idea, as it causes only the third and following files to be copied
-                        elif q == "n":
-                                not_necessary_to_query.append(legacy)
+        elif count == 3:
+                q = input("Is " + legacy + " an artist?\n y/n")
+                if q == "y":
+                        print("Nice we´ll be creating a folder for you")
+                        # add artist to database inorder to run comparison with database again and copy all those whose artist was just added
+                        databaseextender(legacy) # MODULE
+                        # directorator(musicdirpath,f,musicdirpath,legacy) - bad idea, as it causes only the third and following files to be copied
+                elif q == "n":
+                        not_necessary_to_query.append(legacy)
 
 global x
 x = 0
