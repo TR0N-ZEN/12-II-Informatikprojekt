29a7ab85772d6449c85421c684e54fe0259c90ef
Theo <theo.reichert7@gmail.com>
2019-03-17 21:28:58 +0100

    23.0

--- musicator.py
+++ musicator.py
@@ -30,31 +30,29 @@ for dirpath, dirnames, filenames in os.walk(musicdirpath):
         for e in artists:
                 if f_name.find(e[1]) != -1:
                         directorator(musicdirpath,f,dirpath,e[1]) # MODULE
-                else:
-                        filenames_for_analyzation.append(f_name) # names of files which had no match with artist entries in database
+                # else:
+                #         filenames_for_analyzation.append(f_name) # names of files which had no match with artist entries in database
 
 pieces = []
 
-for e in filenames_for_analyzation:
-        z = f_name.split(" ") # pieces is an array / 2D list, 1st dimension elements are lists which contain (2nd dimension) strings which originate from the the entries in filenames_for_analyzation
-        pieces.append(z)
-
-# for e in os.scandir(musicdirpath):
-#         filenames_for_analyzation.append(e.name)
-#         z = e.name.split(" ") # returns a list of strings which in the the string e.name were "seperated" by space
-#         print(z)
+# for e in filenames_for_analyzation:
+#         z = f_name.split(" ") # pieces is an array / 2D list, 1st dimension elements are lists which contain (2nd dimension) strings which originate from the the entries in filenames_for_analyzation
 #         pieces.append(z)
+
+for e in os.scandir(musicdirpath):
+        filenames_for_analyzation.append(e.name)
+        z = e.name.split(" ") # returns a list of strings which in the the string e.name were "seperated" by space
+        pieces.append(z)
 
 global count
 count = 0
-global final
-final = []
 global not_necessary_to_query
 not_necessary_to_query = []
 
 def analyzer(f,r,compound_guess,nextpart,count,legacy):
         if f.find(compound_guess) != -1 and compound_guess not in not_necessary_to_query:
                 legacy = compound_guess # savin the confirmed / approved compound_guess for next recursion so that if it fails it will go to else and return legacy, the last ompound_guess that worked
+                print(legacy)
                 i = r.index(nextpart) # getting index of nextpart
                 nextpart = r[i+1] # redefining nextpart as the item following the current nextpart in list r
                 compound_guess = compound_guess + " " + nextpart
@@ -69,14 +67,10 @@ def analyzer(f,r,compound_guess,nextpart,count,legacy):
                 elif q == "n":
                         not_necessary_to_query.append(legacy)
 
-global i
-i = 0
-
 def start_analyzer(filenames, pieces):
         for f in filenames:
                 for r in pieces:
                         if pieces.index(r) != filenames.index(f) and f.find(r[0]) != -1: # r of pieces and f of filenames with the same index suggests that r is a derivative of f which causes a 100% match causing problems in the matching mechanism 
                                 analyzer(f,r,r[0],r[0],0,"") # passed variables: f = filename; r = list of filenamesubstrings which are followed by a space; r[0]; r[1]; count = 0; legacy is an empty string as there is no legacy value
-        print(final)
 
 start_analyzer(filenames_for_analyzation,pieces)
